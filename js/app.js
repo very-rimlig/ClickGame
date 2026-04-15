f26cfec14de358fc17828e3bf18068b9bcb47614
very-rimlig <josephine.bergenmar@gmail.com>
2026-04-15 13:40:44 +0200

    update

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
 console.log("Hello World!")
+//click a button or object so I can earn points so that i can increase my score
+//se my current score during the game so that i know how well i am doing
+//see a countdown timer so that I know how much time is left
+//Variables
+let score = 0;
+
+// HTML DOM
+const button1 = document.getElementById("button1");
+
+// UI Functions
+button1.addEventListener("click",  () => {
+  increaseScore();
+})
+
+// Functions
+function increaseScore() {
+  score++;
+  console.log(score);
+}
