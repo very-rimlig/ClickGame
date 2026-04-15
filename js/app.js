96e54f9c291353c330b9b622b54f6f976e089969
very-rimlig <josephine.bergenmar@gmail.com>
2026-04-15 14:45:15 +0200

    update

--- js/app.js
+++ js/app.js
@@ -1,12 +1,17 @@
 console.log("Hello World!")
 //click a button or object so I can earn points so that i can increase my score
 //se my current score during the game so that i know how well i am doing
-//see a countdown timer so that I know how much time is left
+//see a countdown timer so that I know how much time is left - setintervall (function)
+
+
 //Variables
 let score = 0;
+let timeLeft = 60;
 
 // HTML DOM
+const timerDisplay = document.getElementById("timerDisplay"); // Hämta klockan
 const button1 = document.getElementById("button1");
+const scoreDisplay = document.getElementById("scoreDisplay");
 
 // UI Functions
 button1.addEventListener("click",  () => {
@@ -16,5 +21,22 @@ button1.addEventListener("click",  () => {
 // Functions
 function increaseScore() {
   score++;
-  console.log(score);
+  scoreDisplay.innerText = score;
 }
+
+function sectimer() {
+  const timerId = setInterval(() => {
+    if (timeLeft <= 0) {
+      clearInterval(timerId);
+      timerDisplay.innerText = "Tiden slut"; // Visa att det är klart
+      console.log("Tiden är ute!");
+    } else {
+      timeLeft--;
+      timerDisplay.innerText = timeLeft; // Uppdatera siffran på skärmen
+      console.log(`Nedräkning: ${timeLeft}`);
+    }
+  }, 1000);
+}
+
+// Kör funktionen för att starta
+sectimer();
